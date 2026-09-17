4e3f7a077b540f4bea22eab932f61a47ce3ff6f2
Siddant Shrestha <siddantshrestha20@gmail.com>
2026-09-17 11:48:39 +0545

    urls and views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(Request):
+    return HttpResponse("home")
+
+def products(Request):
+    return HttpResponse("products")
+
+def customer(Request):
+    return HttpResponse("customer")
