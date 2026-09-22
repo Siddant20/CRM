1f5b8195be5b55457d077604d3350250b4326e8e
Siddant Shrestha <siddantshrestha20@gmail.com>
2026-09-22 12:28:21 +0545

    add order and products models and migrated

--- accounts/models.py
+++ accounts/models.py
@@ -10,4 +10,26 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Product(models.Model):
+    CATEGORY = (
+        ("Indoor", "Indoor"),
+        ("Outdoor", "Outdoor"),
+    )
 
+    name = models.CharField(max_length= 255, null = True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length= 255, null = True, choices= CATEGORY)
+    description = models.CharField(max_length= 255, null = True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+class Order(models.Model):
+    STATUS = (
+        ("Pending", "Pending"),
+        ("Out for delivery", "Out for delivery"),
+        ("Delivered", "Delivered")
+    )
+
+    # Customer
+    # Product
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=255,null= True, choices=STATUS)
