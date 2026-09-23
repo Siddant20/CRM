c237bb43dcf7db3ae6e53deb989ebc724576e862
Siddant Shrestha <siddantshrestha20@gmail.com>
2026-09-23 17:23:41 +0545

    url paths update and delete

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.products, name="products"),
     path('customer/<str:cust_id>/', views.customer, name="customer"),
+    path('create_order/', views.createOrder, name="createOrder"),
+    path('update_order/<str:order_id>', views.updateOrder, name="updateOrder"),
+    path('delete_order/<str:order_id>', views.deleteOrder, name="deleteOrder"),
+
 
 ]
 
