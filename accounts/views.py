b3544d966a0f9f2fcc4fdab9d208fd2162155275
Siddant Shrestha <siddantshrestha20@gmail.com>
2026-09-23 14:25:16 +0545

    retrieve customer from url and pass to template

--- accounts/views.py
+++ accounts/views.py
@@ -17,5 +17,8 @@ def products(request):
     products=Product.objects.all()
     return render(request, "accounts/products.html",{'products': products})
 
-def customer(request):
-    return render(request,"accounts/customer.html")
+def customer(request, cust_id):
+    customer = Customer.objects.get(id=cust_id)
+    orders = customer.order_set.all()
+    context = {'customer':customer, 'orders': orders}
+    return render(request,"accounts/customer.html", context)
