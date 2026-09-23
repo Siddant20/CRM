a673031253f9bea6266b60794244db91b4d0fcf6
Siddant Shrestha <siddantshrestha20@gmail.com>
2026-09-23 17:24:38 +0545

    form actions

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 
 # Create your views here.
 def home(request):
@@ -22,3 +23,35 @@ def customer(request, cust_id):
     orders = customer.order_set.all()
     context = {'customer':customer, 'orders': orders}
     return render(request,"accounts/customer.html", context)
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, order_id):
+    order= Order.objects.get(id=order_id)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, order_id):
+    order= Order.objects.get(id=order_id)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    context = {'item':order}
+    return render(request, 'accounts/delete.html', context )
