592b2e8888b7c835c2ecca44ca69804bf33f0221
Siddant Shrestha <siddantshrestha20@gmail.com>
2026-09-23 14:45:24 +0545

    add url names

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/<str:cust_id>/', views.customer),
+    path('', views.home, name="home"),
+    path('products/', views.products, name="products"),
+    path('customer/<str:cust_id>/', views.customer, name="customer"),
 
 ]
 
